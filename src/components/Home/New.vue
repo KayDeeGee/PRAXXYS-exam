c8088b467b86249186c6bfdb96a53033177f082f
KayDeeGee <kylebryanbasco@gmail.com>
2024-09-13 18:07:00 +0800

    cards for popular and maybe for food menu

--- src/components/Home/New.vue
+++ src/components/Home/New.vue
@@ -1,19 +1,37 @@
 <template>
     <div>
-        
+        <h2>What's New?</h2>
+        <img :src="`${newMenu[0]?.imgUrl}?t=${Date.now()}`" alt="Image not found">
     </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+
 export default {
     setup () {
-        
+        const newMenu = ref([]);
 
-        return {}
+        const fetchWhatsNew = async () => {
+            try {
+                const response = await fetch('/data/new-menu.json'); // Fetch from public/data
+                newMenu.value = await response.json();
+            } catch (error) {
+                console.error('Error fetching categories:', error);
+            }
+        };
+
+        onMounted(() => {
+            fetchWhatsNew();
+        });
+
+        return {newMenu}
     }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+img {
+    width: 100%
+}
 </style>
--- src/components/Home/Popular.vue
+++ src/components/Home/Popular.vue
@@ -1,20 +1,89 @@
 <template>
     <div>
         <h2>Most Popular</h2>
-        
+        <ion-grid class="ion-no-padding">
+            <ion-row>
+                <ion-col v-for="item in popularItems" size="6" class="">
+                    <div class="custom-card">
+                        <div class="img-container">
+                            <img :src="`${item.imgUrl}`" alt="Image not found">
+                        </div>
+                        <div class="card-body ion-padding">
+                            <h2 class="card-title">{{ item.name }}</h2>
+                            <h4 class="card-subtitle">{{ item.category }}</h4>
+                            <ion-row class="ion-justify-content-between">
+                                <h3 class="card-price">P {{ item.price }}</h3>
+                                rating
+                            </ion-row>
+                        </div>
+                    </div>
+                </ion-col>
+            </ion-row>
+        </ion-grid>
     </div>
 </template>
 
 <script>
-export default {
-    setup () {
-        
+import { onMounted, ref } from 'vue';
 
-        return {}
+export default {
+    setup() {
+        const popularItems = ref([]);
+        const fetchPopular = async () => {
+            try {
+                const response = await fetch('/data/popular-items.json'); // Fetch from public/data
+                popularItems.value = await response.json();
+            } catch (error) {
+                console.error('Error fetching popularItems:', error);
+            }
+        };
+
+        onMounted(() => {
+            fetchPopular();
+        });
+        return { popularItems }
     }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.img-container {
+    height: 136px;
+    width: 136px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px auto 10px auto;
+    
+}
 
+.custom-card {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: #E9ECEF;
+    border-radius: 16px;
+}
+
+ion-col {
+    padding: 3px;
+}
+
+.card-title{
+    font-weight: bold;
+    font-size: 14px;
+    margin: 0;
+}
+
+.card-subtitle {
+    font-size: 12px;
+    margin: 0.5rem 0 1rem 0;
+}
+
+.card-price {
+    font-weight: bold;
+    font-size: 16px;
+    margin: 0;
+    color: #D71921;
+}
 </style>
