381841b3d58f14a463cad449be7fc097b30fd874
KayDeeGee <kylebryanbasco@gmail.com>
2024-09-15 19:26:46 +0800

    alert style improved, checkout template

--- src/components/AlertOrderAdded.vue
+++ src/components/AlertOrderAdded.vue
@@ -1,13 +1,49 @@
 <template>
-     <ion-alert trigger="present-alert" header="Successfully Added!" sub-header="What do you want to do now?"
-     :message="alertMessage" :buttons="alertButtons">
-     
+    <ion-alert trigger="present-alert" 
+        :message="alertMessage" :buttons="alertButtons">
     </ion-alert>
 </template>
 
-<script  setup>
+<script setup>
 import { IonAlert } from '@ionic/vue';
+import { useRouter } from 'vue-router';
 
-const alertButtons = ['Action'];
+const router = useRouter();
+
+const alertButtons = [
+    {
+        text: 'Proceed to Checkout',
+        role: 'confirm',
+
+        handler: () => {
+            // store here, cart data
+            router.push('/checkout');
+        },
+    },
+    {
+        text: 'Add More',
+        role: 'confirm',
+        handler: () => {
+            router.back();
+        },
+    },
+];
+
+const alertMessage = 'Successfully Added!\n\nYour order has been added to the cart.'
 
 </script>
+
+<style>
+.alert-head {
+    background-image: url('svg/positive-vote 1.svg');
+    background-repeat: no-repeat;
+    background-position: center;
+    margin: 1rem 0;
+    height: 72px;
+}
+
+.alert-message {
+    white-space: pre-wrap;
+    font-weight: bold;
+}
+</style>
---
+++ src/views/Checkout.vue
@@ -0,0 +1,12 @@
+<template>
+    <ion-page>
+        <ion-content class="ion-padding">
+            putanginaaa
+            
+        </ion-content>
+        <ion-footer></ion-footer>
+    </ion-page>
+</template>
+
+<script setup>
+</script>
